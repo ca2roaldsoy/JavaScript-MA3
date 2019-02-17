b21cad1413d88ecb8e43de7444f9e91821fec309
ca2roaldsoy <44968868+ca2roaldsoy@users.noreply.github.com>
2019-02-17 23:12:12 +0100

    Commit Q2

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-//Use RegEx to validate form
-document.getElementById("submit").addEventListener("click", function() {
-                    
-    var userInput = document.getElementsByTagName("input");
-                
-        for(var i=0; i < userInput.length; i++){
-            var userValue = userInput[i].value.trim();
-            var userRegEx = new RegExp (userInput[i].pattern);
-            var userName = userRegEx.test(userValue);
-                    
-        if(userName == ""){
-            console.log(userInput[i].id + " is invalid")
-                
-        } else {
-            console.log (userValue)
-                
-            } 
-        }
-    })
+//Console.log out elements in JSON file
+var jsonData = {
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+};
+
+var jsonArray = jsonData.video;
+
+console.log("video:");
+         
+for(var i=0; i < jsonArray.length; i++) {
+    
+    var myData = jsonArray[i];
+    console.log("id: " + myData.id);
+    console.log("name: " + myData.name);
+    console.log("url: " + myData.url)
+}
+console.log("")
+
+console.log("author:", "data:")
+    
+for(var l=0; l < myData.author.data.length; l++) { 
+    
+    var dataArray = myData.author.data[l];
+    console.log("name_author: " + dataArray.name_author);
+    console.log("url: " + dataArray.url);
+    console.log("type: " + dataArray.type)
+}
+            
